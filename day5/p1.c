bb56ce0add36cf7319cbc1ec1cba6d5a3c6c09c1
Jeetbhuva1019 <jeetbhuva70@gmail.com>
2024-03-07 15:28:26 +0530

    c

--- day5/p1.c
+++ day5/p1.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
 main(){
-    
+    float a,b,c;
+    printf("enter the value : ");
+    scanf("%f",&a);
+    printf("enter the discount : ");
+    scanf("%f",&b);
+
+    c=a*(b/100);
+    printf("ans : %.2f",c);
 }
---
+++ day5/p3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main(){
+    int a,b,c;
+    printf("enter the value : ");
+    scanf("%d",&a);
+    printf("enter the value : ");
+    scanf("%d",&b);
+    c=180-a-b;
+    printf("ans : %d",c);
+}
